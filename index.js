cf7e9e0a5ca94d5ba4a5bd8a4f63970a2b8ac9f3
Matheus Santos <roxx77344@gmail.com>
2020-06-18 18:34:35 -0300

    command handler adicionado

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const { Client } = require("discord.js");
-const client = new Client();
+const Discord = require("discord.js");
 const config = require("./config.json");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const botCommands = require("./handler");
 
+Object.keys(botCommands).map(key => {
+    client.commands.set(botCommands[key].name, botCommands[key]);
+});
+
+client.on('ready', ()=>{
+    console.log(`${client.user.username} está online`);
+    client.user.setActivity("a pica nelas");
+})
+
+client.on('message', async message =>{
+    const prefix = config.prefix;
+
+    if(!message.content.startsWith(prefix)) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift()
+    if (!client.commands.has(command)) return;
+    
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('erro ao executar o comando');
+    }
+
+})
 
 client.login(config.token);
