ee578cd80d653f2efa9c6c50815dab06b18e8dd4
Matheus Santos <roxx77344@gmail.com>
2020-06-20 15:27:36 -0300

    a

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('ready', ()=>{
     client.user.setActivity("a pica nelas");
     setInterval(()=>{
         client.commands.get("rastreio").editarMsg(client,con)
-    },10000);
+    },1800000);
 })
 
 function generateXp(){
